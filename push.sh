103cf66f7f8246da854ddcf1ba627d9131c523f7
xiaotuan <583168425@qq.com>
2021-02-18 18:28:31 +0800

    添加笔记

--- push.sh
+++ push.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
 git st
-
 git add -A
-
 git commit -m "添加笔记"
-
+echo "开始更新仓库...."
 git pull --rebase
-
+pullStatus = $?
+echo "开始上传代码至Gitee远程仓库...."
 git push origin
-
-git push GitHub
-
+echo "开始上传代码至Github远程仓库...."
+git push Github
+echo "开始上传代码至Gitlab远程仓库...."
 git push Gitlab
